802a37cdd147aa2a7f10f3441133753dc2a02515
soolzana <info@global-aeit.com>
2018-02-15 09:27:35 +0100

    Add set_active_route helper

--- app/helpers.php
+++ app/helpers.php
@@ -11,4 +11,14 @@
        	}
        }
   }
+
+ if(! function_exists('set_active_route')){
+       function set_active_route($route){
+         return Route::is($route) ? 'active' : '';
+       }
+  }
+
+
+
+
 ?>
